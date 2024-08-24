9f5cdee3c9596b2d54490309db7cc20f8a0c0834
hanxiaomax@qq.com <hanxiaomax@qq.com>
2024-08-24 22:10:37 +0800

    dynamic render tabs

--- src/components/BottomDrawer.tsx
+++ src/components/BottomDrawer.tsx
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { Drawer, Tabs, Tab, Button, Box, IconButton } from "@mui/material";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import AddIcon from "@mui/icons-material/Add";
-import SensorsIcon from "@mui/icons-material/Sensors"; // 代表 USS 传感器
-import RadarIcon from "@mui/icons-material/Radar"; // 代表雷达传感器
-import CameraAltIcon from "@mui/icons-material/CameraAlt"; // 代表摄像头传感器
-import ToysIcon from "@mui/icons-material/Toys"; // 代表激光雷达传感器
+import SensorsIcon from "@mui/icons-material/Sensors";
+import RadarIcon from "@mui/icons-material/Radar";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
+import ToysIcon from "@mui/icons-material/Toys";
+
+import sensorData from "../sensor_stocks.json"; // 引入 JSON 文件
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -34,9 +36,10 @@ const TabPanel: React.FC<TabPanelProps> = ({
 
 interface SensorProps {
   icon: React.ReactElement;
+  name: string;
 }
 
-const Sensor: React.FC<SensorProps> = ({ icon }) => {
+const Sensor: React.FC<SensorProps> = ({ icon, name }) => {
   return (
     <Box
       sx={{
@@ -63,6 +66,29 @@ const BottomDrawer: React.FC = () => {
     setSelectedTab(newValue);
   };
 
+  const renderSensors = (sensorType: keyof typeof sensorData) => {
+    return sensorData[sensorType].sensors.map((sensor) => {
+      let icon;
+      switch (sensor.type) {
+        case "uss":
+          icon = <SensorsIcon sx={{ fontSize: "40px" }} />;
+          break;
+        case "lidar":
+          icon = <ToysIcon sx={{ fontSize: "40px" }} />;
+          break;
+        case "radar":
+          icon = <RadarIcon sx={{ fontSize: "40px" }} />;
+          break;
+        case "camera":
+          icon = <CameraAltIcon sx={{ fontSize: "40px" }} />;
+          break;
+        default:
+          icon = <SensorsIcon sx={{ fontSize: "40px" }} />;
+      }
+      return <Sensor key={sensor.id} icon={icon} name={sensor.name} />;
+    });
+  };
+
   return (
     <>
       {/* 扁平按钮，使用图标提示可展开 */}
@@ -73,8 +99,8 @@ const BottomDrawer: React.FC = () => {
           bottom: 0,
           left: "50%",
           transform: "translateX(-50%)",
-          width: "100vw", // 设置按钮宽度为屏幕宽度的60%
-          height: "40px", // 增大按钮的高度
+          width: "100vw",
+          height: "40px",
           backgroundColor: "rgb(253,245,230,0.3)",
           borderRadius: "10px 10px 0 0",
           textAlign: "center",
@@ -83,7 +109,7 @@ const BottomDrawer: React.FC = () => {
           alignItems: "center",
         }}
       >
-        <ExpandLessIcon sx={{ fontSize: "24px" }} /> {/* 调整图标大小 */}
+        <ExpandLessIcon sx={{ fontSize: "24px" }} />
       </Button>
 
       {/* 抽屉面板，背景不灰掉 */}
@@ -108,113 +134,39 @@ const BottomDrawer: React.FC = () => {
           aria-label="sensor types"
           variant="fullWidth"
         >
-          <Tab label="USS Sensors" />
-          <Tab label="Lidar Sensors" />
-          <Tab label="Radar Sensors" />
-          <Tab label="Camera Sensors" />
+          {Object.keys(sensorData).map((key, index) => (
+            <Tab
+              key={index}
+              label={sensorData[key as keyof typeof sensorData].title}
+            />
+          ))}
         </Tabs>
 
-        {/* USS Sensors Tab */}
-        <TabPanel value={selectedTab} index={0}>
-          <Box display="flex" flexWrap="wrap">
-            <Sensor icon={<SensorsIcon sx={{ fontSize: "40px" }} />} />
-            <Sensor icon={<SensorsIcon sx={{ fontSize: "40px" }} />} />
-            <Sensor icon={<SensorsIcon sx={{ fontSize: "40px" }} />} />
-            {/* 添加传感器按钮 */}
-            <Box
-              sx={{
-                width: "80px",
-                height: "80px",
-                backgroundColor: "#e0e0e0",
-                borderRadius: "16px",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                margin: "8px",
-                cursor: "pointer",
-              }}
-            >
-              <IconButton>
-                <AddIcon sx={{ fontSize: "40px" }} />
-              </IconButton>
+        {Object.keys(sensorData).map((key, index) => (
+          <TabPanel key={index} value={selectedTab} index={index}>
+            <Box display="flex" flexWrap="wrap">
+              {renderSensors(key as keyof typeof sensorData)}
+              {/* 添加传感器按钮 */}
+              <Box
+                sx={{
+                  width: "80px",
+                  height: "80px",
+                  backgroundColor: "#e0e0e0",
+                  borderRadius: "16px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  margin: "8px",
+                  cursor: "pointer",
+                }}
+              >
+                <IconButton>
+                  <AddIcon sx={{ fontSize: "40px" }} />
+                </IconButton>
+              </Box>
             </Box>
-          </Box>
-        </TabPanel>
-
-        {/* Lidar Sensors Tab */}
-        <TabPanel value={selectedTab} index={1}>
-          <Box display="flex" flexWrap="wrap">
-            <Sensor icon={<ToysIcon sx={{ fontSize: "40px" }} />} />
-            <Sensor icon={<ToysIcon sx={{ fontSize: "40px" }} />} />
-            <Box
-              sx={{
-                width: "80px",
-                height: "80px",
-                backgroundColor: "#e0e0e0",
-                borderRadius: "16px",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                margin: "8px",
-                cursor: "pointer",
-              }}
-            >
-              <IconButton>
-                <AddIcon sx={{ fontSize: "40px" }} />
-              </IconButton>
-            </Box>
-          </Box>
-        </TabPanel>
-
-        {/* Radar Sensors Tab */}
-        <TabPanel value={selectedTab} index={2}>
-          <Box display="flex" flexWrap="wrap">
-            <Sensor icon={<RadarIcon sx={{ fontSize: "40px" }} />} />
-            <Sensor icon={<RadarIcon sx={{ fontSize: "40px" }} />} />
-            <Box
-              sx={{
-                width: "80px",
-                height: "80px",
-                backgroundColor: "#e0e0e0",
-                borderRadius: "16px",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                margin: "8px",
-                cursor: "pointer",
-              }}
-            >
-              <IconButton>
-                <AddIcon sx={{ fontSize: "40px" }} />
-              </IconButton>
-            </Box>
-          </Box>
-        </TabPanel>
-
-        {/* Camera Sensors Tab */}
-        <TabPanel value={selectedTab} index={3}>
-          <Box display="flex" flexWrap="wrap">
-            <Sensor icon={<CameraAltIcon sx={{ fontSize: "40px" }} />} />
-            <Sensor icon={<CameraAltIcon sx={{ fontSize: "40px" }} />} />
-            <Box
-              sx={{
-                width: "80px",
-                height: "80px",
-                backgroundColor: "#e0e0e0",
-                borderRadius: "16px",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                margin: "8px",
-                cursor: "pointer",
-              }}
-            >
-              <IconButton>
-                <AddIcon sx={{ fontSize: "40px" }} />
-              </IconButton>
-            </Box>
-          </Box>
-        </TabPanel>
+          </TabPanel>
+        ))}
       </Drawer>
     </>
   );
