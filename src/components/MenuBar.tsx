976d19107381ee9affb55deb3b6cd7345e4c85e5
hanxiaomax@qq.com <hanxiaomax@qq.com>
2024-09-04 22:23:13 +0800

    add about page

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,5 +1,20 @@
-import React from "react";
-import { AppBar, Button, ButtonGroup } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Button,
+  ButtonGroup,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Avatar,
+  Typography,
+  Link,
+  Box,
+  IconButton,
+} from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
 import ProfileMenu from "../components/ProfileMenu";
 import ViewMenu from "../components/ViewMenu";
 
@@ -18,44 +33,107 @@ const MenuBar: React.FC<MenuBarProps> = ({
   uiConfig,
   setUiConfig,
 }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleAboutOpen = () => {
+    setOpen(true);
+  };
+
+  const handleAboutClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <AppBar
-      position="static"
-      elevation={0}
-      sx={{
-        backgroundColor: "#ffffff",
-        height: "10px",
-      }}
-    >
-      <ButtonGroup
-        disableElevation
-        variant="text"
-        size="large"
+    <>
+      <AppBar
+        position="static"
+        elevation={0}
         sx={{
-          "& .MuiButtonBase-root": {
-            borderColor: "#f6f6f6",
-            color: "#0c7a92",
-            borderRadius: 0,
-            fontWeight: "bold", // 设置字体为粗体
-            textTransform: "none", // 禁用大写转换
-            "&:hover": {
-              backgroundColor: "#0c7a92",
-              color: "white",
-            },
-          },
+          backgroundColor: "#ffffff",
+          height: "10px",
         }}
       >
-        <ProfileMenu
-          onImportSensorSetConfigImport={handleSensorSetConfigImport}
-          onImportSensorStock={handleSensorStockImport}
-          onExport={handleExport}
-        />
-        <ViewMenu uiConfig={uiConfig} setUiConfig={setUiConfig} />
-        <Button>Tools</Button>
-        <Button>Snapshot</Button>
-        <Button>About</Button>
-      </ButtonGroup>
-    </AppBar>
+        <ButtonGroup
+          disableElevation
+          variant="text"
+          size="large"
+          sx={{
+            "& .MuiButtonBase-root": {
+              borderColor: "#f6f6f6",
+              color: "#0c7a92",
+              borderRadius: 0,
+              fontWeight: "bold",
+              textTransform: "none",
+              "&:hover": {
+                backgroundColor: "#0c7a92",
+                color: "white",
+              },
+            },
+          }}
+        >
+          <ProfileMenu
+            onImportSensorSetConfigImport={handleSensorSetConfigImport}
+            onImportSensorStock={handleSensorStockImport}
+            onExport={handleExport}
+          />
+          <ViewMenu uiConfig={uiConfig} setUiConfig={setUiConfig} />
+          <Button>Tools</Button>
+          <Button>Snapshot</Button>
+          <Button onClick={handleAboutOpen}>About</Button>
+        </ButtonGroup>
+      </AppBar>
+
+      {/* Dialog for About information */}
+      <Dialog open={open} onClose={handleAboutClose}>
+        <DialogTitle>About</DialogTitle>
+        <DialogContent>
+          <Avatar
+            src="https://avatars.githubusercontent.com/u/3370445?v=4"
+            alt="Author's Avatar"
+            sx={{ width: 80, height: 80, mb: 2 }}
+          />
+          <Typography variant="h3">ADAS Sensor Set Builder</Typography>
+          <Typography variant="overline">
+            A handy tool for ADAS Product Managers and System Engineers
+          </Typography>
+          <Box height={50}></Box>
+
+          <Box display="flex" alignItems="center" mb={1}>
+            <GitHubIcon sx={{ mr: 1 }} />
+            <Typography variant="body2">
+              <Link
+                href="https://github.com/hanxiaomax"
+                target="_blank"
+                rel="noopener"
+              >
+                Lingfeng AI
+              </Link>
+            </Typography>
+          </Box>
+
+          <Box display="flex" alignItems="center" mb={1}>
+            <EmailIcon sx={{ mr: 1 }} />
+            <Typography variant="body2">hanxiaomax@qq.com</Typography>
+          </Box>
+
+          <Box display="flex" alignItems="center" mb={1}>
+            <GitHubIcon sx={{ mr: 1 }} />
+            <Typography variant="body2">
+              <Link
+                href="https://github.com/hanxiaomax/vehicle-sensorset-builder"
+                target="_blank"
+                rel="noopener"
+              >
+                GitHub Project
+              </Link>
+            </Typography>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleAboutClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
